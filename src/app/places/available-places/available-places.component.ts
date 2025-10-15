9813c419abac20522e864fa40cdf541536b52194
codetivated <melisa@heying.us>
2025-10-15 10:28:41 -0500

    show alternative approach to response data

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -33,3 +33,41 @@ export class AvailablePlacesComponent implements OnInit {
     });
   }
 }
+
+// import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+// import { Place } from '../place.model';
+// import { PlacesComponent } from '../places.component';
+// import { PlacesContainerComponent } from '../places-container/places-container.component';
+// import { HttpClient } from '@angular/common/http';
+
+// @Component({
+//   selector: 'app-available-places',
+//   standalone: true,
+//   templateUrl: './available-places.component.html',
+//   styleUrl: './available-places.component.css',
+//   imports: [PlacesComponent, PlacesContainerComponent],
+// })
+// export class AvailablePlacesComponent implements OnInit {
+//   places = signal<Place[] | undefined>(undefined);
+//   private httpClient = inject(HttpClient);
+//   private destroyRef = inject(DestroyRef);
+
+//   ngOnInit() {
+//     const subscription = this.httpClient
+//       .get<{ places: Place[] }>('http://localhost:3000/places', {
+//         observe: 'response',
+//       })
+//       .subscribe({
+//         next: (response) => {
+//           console.log(response);
+//           console.log(response.body?.places);
+//           this.places.set(response.body?.places);
+//         },
+//       });
+//     this.destroyRef.onDestroy(() => {
+//       console.log('AvailablePlacesComponent destroyed');
+//       subscription.unsubscribe();
+//     });
+//   }
+// }
