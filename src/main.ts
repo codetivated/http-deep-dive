aabbef70067e30d4240a1097cdc198e767f1f77f
codetivated <melisa@heying.us>
2025-10-16 08:59:07 -0500

    minor example of using http interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+
+function loggerInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
+  console.log('HTTP Request:', req);
+  return next(req);
+}
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(withInterceptors([loggerInterceptor]))],
 }).catch((err) => console.error(err));
+
+// example of modifying request headers
+// function loggerInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
+//   const request = req.clone({
+//     headers: req.headers.set('X-DEBUG', 'TESTING'),
+//   });
+//   console.log('HTTP Request:', request);
+//   return next(request);
+// }
